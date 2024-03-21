017fe4a3f6c2f8fd8a725365e2af2c34f3cd4ac7
Dst07032023 <bovari.deonisis@gmail.com>
2024-03-21 19:18:43 +0100

    initial commit and push

---
+++ src/test/java/com/bookit/step_defenitions/UIStepDefs.java
@@ -0,0 +1,23 @@
+package com.bookit.step_defenitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class UIStepDefs {
+    @Given("User logs in with {string} and {string}")
+    public void user_logs_in_with_and(String string, String string2) {
+
+    }
+
+    @When("User navigates to mySelf page")
+    public void user_navigates_to_mySelf_page() {
+
+    }
+
+    @Then("User gets the UI info")
+    public void user_gets_the_UI_info() {
+
+    }
+
+}
